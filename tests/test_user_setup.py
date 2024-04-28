adcb88eaae445270b1e912a61ca31a648be90428
diddychriss <krzysztof.zak@stxnext.pl>
2024-04-28 13:11:51 +0200

    [ZD-10] lint tests

--- tests/test_user_setup.py
+++ tests/test_user_setup.py
@@ -9,17 +9,13 @@ class UserSetup:
     @pytest.fixture
     def admin_user(self):
         return User.objects.create_superuser(
-            username='admin',
-            password='adminpass',
-            email='admin@example.com'
+            username="admin", password="adminpass", email="admin@example.com"
         )
 
     @pytest.fixture
     def non_admin_user(self):
         return User.objects.create_user(
-            username='user',
-            password='userpass',
-            email='user@example.com'
+            username="user", password="userpass", email="user@example.com"
         )
 
     @pytest.fixture
